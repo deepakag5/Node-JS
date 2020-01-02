24506020f956d3d0af0171fbe3082443461bd8ae
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-01 23:38:02 -0500

    added success and error messages

--- nodejs_basics/promises.js
+++ nodejs_basics/promises.js
@@ -6,6 +6,15 @@ doStuff(true, (result) => console.log(result));
 
 function doStuff(data) {
     return new Promise((resolve, reject) => {
+        let successMessage = {
+            status: 'success',
+            message: 'All is well!'
+        };
+
+        let errorMessage = {
+            status: 'error',
+            message: 'Oops, there was an error!'
+        };
 
         if (typeof data === 'boolean' && data === true) {
             resolve(successMessage);
@@ -15,3 +24,11 @@ function doStuff(data) {
     });
 }
 
+doStuff(true).then(
+    (successMessage) => {
+        console.log(successMessage);
+    },
+    (errorMessage) => {
+        console.log(errorMessage);
+    }
+);
