e751da843c5620f38e731c4c580acf187a1f2093
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-01 22:27:03 -0500

    added string and var operations in app.js file

--- nodejs_basics/app.js
+++ nodejs_basics/app.js
@@ -1,1 +1,17 @@
-console.log("Hello World")
+// print a string
+console.log("Hello World");
+
+//print a string using variables
+var x = "Hello";
+var y = "World";
+
+console.log(x + " " + y);
+
+// assign and perform operations on variables
+var a = 10;
+var b = 20;
+var c = 5;
+
+console.log((a + b) * c);
+
+console.log(a > b);
