5b7a4b03ca2d71e3220bb94075e9c14bd4d65d4e
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-01 22:29:35 -0500

    added undefined and const type examples

--- nodejs_basics/app.js
+++ nodejs_basics/app.js
@@ -15,3 +15,14 @@ var c = 5;
 console.log((a + b) * c);
 
 console.log(a > b);
+
+// undefined type
+p = undefined;
+
+console.log(p);
+
+// constant type
+const z = 5;
+
+// below will throw an error as we cannot reassign to a constant var
+//z = 6;
