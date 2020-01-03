8a6173b0846c9f06fc88492e1ecfe295dcb44099
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-02 23:55:00 -0500

    creating basic webserver using express - complete server.js file

--- basic_webserver/server.js
+++ basic_webserver/server.js
@@ -1,2 +1,18 @@
 // create package.json using command - npm init
 // also install the module express using command - npm install express --save
+
+// reference the express module
+const express = require('express');
+
+// create an app
+const app = express();
+
+// add a route
+app.get('/', (req, res) => {
+    res.send("Hello World, this is nodejs web server !");
+});
+
+// specify the port
+app.listen(3000, () => {
+    console.log("Web server listening on port 3000 !");
+})
