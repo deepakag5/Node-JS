03be3cbfa0e7f343adce721c516a7cd3c1dacd8b
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-03 23:48:58 -0500

    added comments in res api file

--- basic_rest_api/restapi.js
+++ basic_rest_api/restapi.js
@@ -5,12 +5,16 @@ const app = express();
 
 // to parse the incoming request from express
 app.use(bodyParser.json());
+
+// access the static web page
 app.use(express.static(__dirname + '/public'));
 
+// get route
 app.get('/', (req, res) => {
     res.send("Hello Rest API!");
 });
 
+// post route
 app.post('/hello', (req, res) => {
     let body = req.body;
     body.message = "Hello " + body.name;
@@ -19,6 +23,7 @@ app.post('/hello', (req, res) => {
     res.json(body);
 });
 
+// listening port
 app.listen(3000, () => {
     console.log("Server listening on port 3000!");
 });
