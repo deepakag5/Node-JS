cba634433d18f37e52a8202c7d7a05416be7b6b5
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-03 23:29:53 -0500

    added body parser and code to handle incoming request from express

--- basic_rest_api/restapi.js
+++ basic_rest_api/restapi.js
@@ -1,5 +1,11 @@
-const express = require('express')
-const app = express()
+// get the required packages
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+
+// to parse the incoming request from express
+app.use(bodyParser.json())
+
 
 app.get('/', (req, res) => {
     res.send("Hello Express!")
