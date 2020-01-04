807d8d6d7d267c2657a0db5c5e5660229a2f5e89
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-03 23:33:36 -0500

    added code to send json reponse back

--- basic_rest_api/restapi.js
+++ basic_rest_api/restapi.js
@@ -4,13 +4,18 @@ const bodyParser = require('body-parser')
 const app = express();
 
 // to parse the incoming request from express
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 
 app.get('/', (req, res) => {
-    res.send("Hello Express!")
-})
+    res.send("Hello Rest API!");
+});
 
 app.post('/hello', (req, res) => {
+    let body = req.body;
+    body.message = "Hello " + body.name;
 
-})
+    // send json back as response
+    res.json(body);
+});
+
