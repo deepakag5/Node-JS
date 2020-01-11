adfe1193b1aae6cbd59507ab1cfb8ce3021817d0
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-11 00:18:31 -0500

    added create table in table operations

--- database_CRUD_operations/table_operations.js
+++ database_CRUD_operations/table_operations.js
@@ -10,8 +10,43 @@ dynamodb.listTables({}, (err, data) => {
     } else {
         console.log(data);
     }
-})
+});
 
+
+dynamodb.createTable({
+    TableName: "test_table_ops",
+    AttributeDefinitions: [
+        {
+            AttributeName: "user_id",
+            AttributeType: "S"
+        },
+        {
+            AttributeName: "timestamp",
+            AttributeType: "N"
+        }
+    ],
+    KeySchema: [
+        {
+            AttributeName: "user_id",
+            KeyType: "HASH"
+        },
+        {
+            AttributeName: "timestamp",
+            KeyType: "RANGE"
+        }
+    ]
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 1,
+        WriteCapacityUnits: 1
+    }
+
+}, (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(JSON.stringify(data, null, 2))
+    }
+});
 
 dynamodb.describeTable({
     TableName: "test_table_ops"
@@ -22,5 +57,7 @@ dynamodb.describeTable({
         console.log(JSON.stringify(data, null, 2));
     }
 });
+
+
 
 
