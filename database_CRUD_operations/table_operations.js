a079fd0f3381026b3c0dc4c14144b548cb163038
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-11 00:19:34 -0500

    added update table in table operations

--- database_CRUD_operations/table_operations.js
+++ database_CRUD_operations/table_operations.js
@@ -59,5 +59,20 @@ dynamodb.describeTable({
 });
 
 
+dynamodb.updateTable({
+    TableName: "td_notes_sdk",
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 2,
+        WriteCapacityUnits: 1
+    }
+}, (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(JSON.stringify(data, null, 2));
+    }
+});
+
+
 
 
