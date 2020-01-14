bfc966c67c49c127d476c6469069ce9154fc2449
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-13 21:23:40 -0500

    added query and scan operation to file read operations

--- database_CRUD_operations/read_operations.js
+++ database_CRUD_operations/read_operations.js
@@ -17,6 +17,31 @@ docClient.get({
     }
 });
 
+docClient.query({
+    TableName: 'test_table_ops',
+    KeyConditionExpression: "user_id= :uid",
+    ExpressionAttributeValues: {
+        ":uid": "A"
+    }
+}, (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
 
 
-
+docClient.scan({
+    TableName: "test_table_ops",
+    FilterExpression: "cat=:cat",
+    ExpressionAttributeValues: {
+        ":cat": "general"
+    }
+}, (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
